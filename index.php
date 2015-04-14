79fada4da0da686c2eca0f7ab950f1b06b2470c7
Zack Neff <zack@zackneff.com>
2015-04-14 15:45:25 -0400

    new

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>UM Schedule Me - Sign In</title>
+  </head>
+  <body id="signin">
+
+
+  <div class="container">
+    <div class="row signin">
+      <img class="logosignin" src="logo.png" />
+      <form role="signin" id="signin" method="post" action="signinProcess.php">
+        <input type="text" id="username" name="username" placeholder="Username" required/>
+        <input type="password" id="password" placeholder="Password" name="password"  required/>
+        <input type="submit" class="signin" value="Sign In" />
+      </form>
+        <span class="forgot-password">
+          <a href="forgotPassword.php"><p>Forgot Password?</p></a>
+        </span>
+        <span class="return-home">
+          <a href="index.php"><p>Return Home</p></a>
+        </span>
+    </div>
+  </div>
+
+</body>
+</html>
